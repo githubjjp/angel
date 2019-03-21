b3e1685248366d6abb48254ca20d15d120c6392a
639419263 <639419263@qq.com>
2019-03-21 16:22:17 +0800

    目录结构出错了

--- angel-coreservice/angel-stream/src/main/java/com/pingan/stream/dao/mongoDao/DeviceOperateDao.java
+++
@@ -1,8 +0,0 @@
-package com.pingan.stream.dao.mongoDao;
-
-import com.pingan.stream.entity.DeviceReportOrderEntity;
-
-
-public interface DeviceOperateDao extends MongoBaseDao<DeviceReportOrderEntity>{
-
-}
