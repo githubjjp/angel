c78f24d31eb2ac8e08472a6fe237ab769ada6d0f
639419263 <639419263@qq.com>
2019-04-04 17:48:21 +0800

    提交一下代码

---
+++ angel-coreservice/angel-stream/src/main/java/com/pingan/stream/mongodb/dao/QcTestSuccessDeviceDao.java
@@ -0,0 +1,7 @@
+package com.pingan.stream.mongodb.dao;
+
+import com.pingan.angel.admin.api.mongodb.QcTestSuccessDeviceEntity;
+
+
+public interface QcTestSuccessDeviceDao extends MongoBaseDao<QcTestSuccessDeviceEntity>{
+}
---
+++ angel-coreservice/angel-stream/src/main/java/com/pingan/stream/mongodb/dao/impl/QcTestSuccessDeviceDaoImpl.java
@@ -0,0 +1,9 @@
+package com.pingan.stream.mongodb.dao.impl;
+
+import com.pingan.angel.admin.api.mongodb.QcTestSuccessDeviceEntity;
+import com.pingan.stream.mongodb.dao.QcTestSuccessDeviceDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class QcTestSuccessDeviceDaoImpl extends MongoBaseDaoImpl<QcTestSuccessDeviceEntity> implements QcTestSuccessDeviceDao {
+}
