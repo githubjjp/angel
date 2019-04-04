711e7ec61ff7715645ba95bd8a267f0271d6f75f
EX-GUOYONG002 <2809931862qq.com>
2019-04-04 11:06:09 +0800

    设备激活

--- angel-coreservice/angel-admin/src/main/java/com/pingan/angel/admin/controller/ProductActiveController.java
+++ angel-coreservice/angel-admin/src/main/java/com/pingan/angel/admin/controller/ProductActiveController.java
@@ -56,7 +56,7 @@ public class ProductActiveController {
 
 
     /**
-     * 设备基本信息查询
+     * 设备在离线查询
      *
      * @param id
      * @return
@@ -68,10 +68,90 @@ public class ProductActiveController {
 
         // md5解密比对
 
+
         // 调用iot服务
 
         return new Result();
     }
 
+
+    /**
+     * 设备控制
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/cmd")
+    public Result cmdDevice(@PathVariable Integer id, @PathVariable String customerSuperCode,
+                            @PathVariable String barcodeId, @PathVariable String logisticsCode,
+                            @PathVariable String sign) {
+
+        // md5解密比对
+
+
+        // 调用iot服务
+
+        return new Result();
+    }
+
+
+    /**
+     * 设备实时信息查询
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/getDeviceNow")
+    public Result getDeviceNow(@PathVariable Integer id, @PathVariable String customerSuperCode,
+                               @PathVariable String barcodeId, @PathVariable String logisticsCode,
+                               @PathVariable String sign) {
+
+        // md5解密比对
+
+
+        // 调用iot服务
+
+        return new Result();
+    }
+
+
+    /**
+     * 滤芯重置api
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/resetFilter")
+    public Result resetFilter(@PathVariable Integer id, @PathVariable String customerSuperCode,
+                              @PathVariable String barcodeId, @PathVariable String logisticsCode,
+                              @PathVariable String sign) {
+
+        // md5解密比对
+
+
+        // 调用iot服务
+
+        return new Result();
+    }
+
+
+    /**
+     *
+     * 用水量查询
+     * @param id
+     * @return
+     */
+    @GetMapping("/useWaterInfo")
+    public Result useWaterInfo(@PathVariable Integer id, @PathVariable String customerSuperCode,
+                              @PathVariable String barcodeId, @PathVariable String logisticsCode,
+                              @PathVariable String sign) {
+
+        // md5解密比对
+
+
+        // 调用iot服务
+
+        return new Result();
+    }
 
 }
