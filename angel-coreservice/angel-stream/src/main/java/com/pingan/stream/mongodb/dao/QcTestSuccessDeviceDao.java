9a80be403ad5ce7d426db79c316ead204ea216aa
639419263 <639419263@qq.com>
2019-04-04 17:49:44 +0800

    提交一下代码

--- angel-coreservice/angel-stream/src/main/java/com/pingan/stream/mongodb/dao/QcTestSuccessDeviceDao.java
+++ angel-coreservice/angel-stream/src/main/java/com/pingan/stream/mongodb/dao/QcTestSuccessDeviceDao.java
@@ -1,7 +1,8 @@
 package com.pingan.stream.mongodb.dao;
 
 import com.pingan.angel.admin.api.mongodb.QcTestSuccessDeviceEntity;
+import com.pingan.angel.common.core.mongodb.MongoBaseDao;
 
 
-public interface QcTestSuccessDeviceDao extends MongoBaseDao<QcTestSuccessDeviceEntity>{
+public interface QcTestSuccessDeviceDao extends MongoBaseDao<QcTestSuccessDeviceEntity> {
 }
