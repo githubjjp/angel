0cd550383133feea77c428bfeeeb3ba8e62bccfa
平安-徐伟 <502496062@qq.com>
2019-04-09 10:27:01 +0800

    修改

--- angel-coreservice/angel-qctest/src/main/java/com/pingan/angel/qctest/service/QcDeviceDispatchService.java
+++ angel-coreservice/angel-qctest/src/main/java/com/pingan/angel/qctest/service/QcDeviceDispatchService.java
@@ -3,6 +3,7 @@ package com.pingan.angel.qctest.service;
 import com.pingan.angel.admin.api.entity.ApiResult;
 import com.pingan.angel.admin.api.mongodb.QcDeviceUnionInfoEntity;
 import com.pingan.angel.admin.api.mongodb.QcTestSuccessDeviceEntity;
+import com.pingan.angel.admin.api.mysql.DeviceEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +14,19 @@ import java.util.Map;
  * 产测api调度服务
  */
 @Service
-public class QCDeviceDispatchService {
+public class QcDeviceDispatchService {
 
     @Autowired
-    private QCTestSuccessServcie qcTestSuccessServcie;
+    private QcTestSuccessServcie qcTestSuccessServcie;
     @Autowired
     private DeviceUnionInfoService deviceUnionInfoService;
+    @Autowired
+    private DeviceService deviceService;
 
 
     /**
      * 判断产测是否通过服务
+     *
      * @param snCode 整机码
      * @return
      */
@@ -30,16 +34,37 @@ public class QCDeviceDispatchService {
         QcTestSuccessDeviceEntity qcSuccessEntity = qcTestSuccessServcie.findBySnCode(snCode);
         //无论产测是否通过都要去查询到对应的配件码
         QcDeviceUnionInfoEntity unionEntity = deviceUnionInfoService.findBySnCode(snCode);
-        if (unionEntity==null){
+        if (unionEntity == null) {
             return ApiResult.error("整机码错误！");
         }
-        HashMap map = new HashMap<String,Object>();
-        map.put("isTestSuccess",qcSuccessEntity!=null?true:false);
-        map.put("barcodeId",unionEntity.getBarcodeId());
+        HashMap map = new HashMap<String, Object>();
+        map.put("isTestSuccess", qcSuccessEntity != null ? true : false);
+        map.put("barcodeId", unionEntity.getBarcodeId());
         return ApiResult.success(map);
     }
 
     public Map<String, Object> scanTest(String snCode, boolean isWifi, String mac) {
+        return null;
+    }
+
+    /**
+     * 重新产测
+     *
+     * @param snCode 整机码
+     * @return
+     */
+    public Map<String, Object> reQcDevice(String snCode) {
+        QcTestSuccessDeviceEntity successDevice = qcTestSuccessServcie.findBySnCode(snCode);
+        if (successDevice != null) {
+            DeviceEntity device = deviceService.findOne(successDevice.getDeviceId());//设备信息
+
+            //修改设备表中的是否产测为否
+
+            //逻辑删除设备表中的设备产测状态
+            //逻辑删除设备状态表中的
+        }else{
+            return ApiResult.error("重新产测失败，该设备未产测过");
+        }
         return null;
     }
 }
