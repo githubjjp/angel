3ab55f0091d3c5cbd23ff18496cc16f0cdddcfcd
ouyang <yakanty@163.com>
2019-04-16 20:40:40 +0800

    修改BUG

--- angel-auth/src/main/java/com/pingan/angel/auth/AngelAuthApplication.java
+++ angel-auth/src/main/java/com/pingan/angel/auth/AngelAuthApplication.java
@@ -14,7 +14,7 @@ import com.pingan.angel.security.annotation.EnableAngelFeignClients;
  */
 @SpringCloudApplication
 @EnableAngelFeignClients(basePackages = {"com.pingan.angel"})
-@ComponentScan("com.pingan.angel")
+@ComponentScan(basePackages="com.pingan.angel")
 public class AngelAuthApplication {
 
 	public static void main(String[] args) {
