b7497bb96123096470ac8bf3a08ee053f4cb569e
ouyang <yakanty@163.com>
2019-04-16 11:14:25 +0800

    修改测试环境地址

--- angel-gateway/src/main/java/com/pingan/angel/gateway/AngelGatewayApplication.java
+++ angel-gateway/src/main/java/com/pingan/angel/gateway/AngelGatewayApplication.java
@@ -2,8 +2,10 @@ package com.pingan.angel.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringCloudApplication
+@ComponentScan(basePackages="com.pingan.angel")
 public class AngelGatewayApplication {
 
 	public static void main(String[] args) {
